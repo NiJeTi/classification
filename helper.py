024f761b93c945bf59e494f06678f2ab44c31a83
NiJeTi <nijeti.inc@gmail.com>
2021-01-18 23:14:53 +0300

    Helper script

---
+++ helper.py
@@ -0,0 +1,36 @@
+import os
+
+import pandas as pd
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def get_data():
+    dataset_path = os.path.join(os.path.dirname(__file__), 'Data.csv')
+    dataset = pd.read_csv(dataset_path)
+
+    x = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
+
+    return x, y
+
+
+def preprocess_data(x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+    scaler = StandardScaler()
+
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    return x_train, x_test, y_train, y_test
+
+
+def calculate_metrics(y_predicted, y_test, name):
+    cm = confusion_matrix(y_test, y_predicted)
+    acc = accuracy_score(y_test, y_predicted)
+
+    print(f'{name}:')
+    print(cm)
+    print(acc)
